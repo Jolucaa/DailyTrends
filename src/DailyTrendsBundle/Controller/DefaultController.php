3626f48fd027592601976b07b699d03708df3258
vagrant <vagrant@homestead>
2018-05-24 22:42:30 +0000

    Symfony Installation

---
+++ src/DailyTrendsBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DailyTrendsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('@DailyTrends/Default/index.html.twig');
+    }
+}
---
+++ src/DailyTrendsBundle/DailyTrendsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DailyTrendsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DailyTrendsBundle extends Bundle
+{
+}
